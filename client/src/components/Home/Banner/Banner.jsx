6dd4e5d1cc4039b53493a4390fae30382374d0b6
ansh-prasad <anshprasad01@gmail.com>
2024-07-22 19:40:35 +0530

    UI

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,25 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Convallis interdum purus adipiscing dis parturient 
+            posuere ac a quam a eleifend montes parturient posuere 
+            curae tempor
+          </p>
+          <dic className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </dic>
+        </div>
+        <img className="banner-img" src={BannerImg} alt="Error" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
