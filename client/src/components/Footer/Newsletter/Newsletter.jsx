0af0a9617c2d2ff53f623043ff68af5e2ff160cd
ansh-prasad <anshprasad01@gmail.com>
2024-07-23 13:31:00 +0530

    NewsLetters Section

--- client/src/components/Footer/Newsletter/Newsletter.jsx
+++ client/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,43 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
+
 import "./Newsletter.scss";
+
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">Sing Up For Latest Updates And Offers</span>
+        <div className="form">
+          <input type="text" placeholder="Email Address" />
+          <button>Subscribe</button>
+        </div>
+        <div className="text">
+          Will be used in accordance with our Privacy Policy
+        </div>
+        <div className="social-icons">
+          <div className="icon">
+            <FaFacebookF siz={14} />
+          </div>
+          <div className="icon">
+            <FaTwitter siz={14} />
+          </div>
+          <div className="icon">
+            <FaInstagram siz={14} />
+          </div>
+          <div className="icon">
+            <FaLinkedinIn siz={14} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
