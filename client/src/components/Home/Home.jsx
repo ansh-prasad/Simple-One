ae09aeee3f50774c70baa26415260a4f839169b5
ansh-prasad <anshprasad01@gmail.com>
2024-07-23 16:36:33 +0530

    Category UI

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,18 @@
 import "./Home.scss";
 
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 const Home = () => {
-    return <div className="home">
-        <Banner/>
-    </div> ;
+  return (
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
