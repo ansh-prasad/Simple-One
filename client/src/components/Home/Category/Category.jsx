6f3da312f942874497165b061ba00e7fc492179f
ansh-prasad <anshprasad01@gmail.com>
2024-07-26 18:03:41 +0530

    API

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,6 +1,9 @@
 import "./Category.scss";
 
 import cat1 from "../../../assets/category/1.png";
+import cat2 from "../../../assets/category/2.png";
+import cat3 from "../../../assets/category/3.png";
+import cat4 from "../../../assets/category/4.png";
 
 const Category = () => {
   return (
@@ -11,13 +14,16 @@ const Category = () => {
           <h1>Ear Burds</h1>
         </div>
         <div className="category">
-          <img src={cat1} alt="" />
+          <img src={cat2} alt="" />
+          <h1>Head Phones</h1>
         </div>
         <div className="category">
-          <img src={cat1} alt="" />
+          <img src={cat3} alt="" />
+          <h1>Speaker</h1>
         </div>
         <div className="category">
-          <img src={cat1} alt="" />
+          <img src={cat4} alt="" />
+          <h1>Smart Watch</h1>
         </div>
       </div>
     </div>
@@ -25,3 +31,7 @@ const Category = () => {
 };
 
 export default Category;
+
+
+
+
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,17 +1,30 @@
 import "./Home.scss";
 
+import { useEffect } from "react";
+
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
 
 const Home = () => {
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories?populate=*").then((res) =>
+      console.log(res)
+    );
+  };
+
   return (
     <div>
       <Banner />
       <div className="main-content">
         <div className="layout">
           <Category />
-          <Products headingText="Popular Products"  />
+          <Products headingText="Popular Products" />
         </div>
       </div>
     </div>
